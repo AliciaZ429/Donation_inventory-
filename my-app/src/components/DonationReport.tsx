8839df5c062eddb46631a53a4855f83a66e4165e
AliciaZ429 <yinging429@163.com>
2024-01-26 20:44:59 -0500

    donation reports tsx done

--- my-app/src/components/DonationReport.tsx
+++ my-app/src/components/DonationReport.tsx
@@ -1,2 +1,38 @@
-const DonationReport: React.FC = () => {};
+import React from "react";
+import { Paper, Typography } from "@mui/material";
+
+interface DonationReportProps {
+  inventoryReport: InventoryReport;
+  donorReport: DonorReport;
+}
+
+interface InventoryReport {
+  [type: string]: number;
+}
+
+interface DonorReport {
+  [donorName: string]: number;
+}
+
+const DonationReport: React.FC<DonationReportProps> = ({
+  inventoryReport,
+  donorReport,
+}) => {
+  return (
+    <Paper>
+      <Typography variant="h5">Inventory Report</Typography>
+      <ul>
+        {Object.entries(inventoryReport).map(([type, quantity]) => {
+          <li key={type}>{`${type}:${quantity}`}</li>;
+        })}
+      </ul>
+      <Typography variant="h5">Donor Report</Typography>
+      <ul>
+        {Object.entries(donorReport).map(([donorName, quantity]) => {
+          <li key={donorName}>{`${donorName}:${quantity}`}</li>;
+        })}
+      </ul>
+    </Paper>
+  );
+};
 export default DonationReport;
